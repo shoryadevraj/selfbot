63efee5e43b7f62abc7466fe13c7bfdee7e6c0c3
shoryadevraj <shoryadevraj@gmail.com>
2025-12-17 13:31:06 +0530

    Create setvclock.js

---
+++ commands/utility/setvclock.js
@@ -0,0 +1,16 @@
+export default {
+  name: "setvclock",
+  aliases: ["vclock"],
+  category: "utility",
+
+  async execute(message, args, client) {
+    if (args.length !== 1) return message.channel.send("```Usage: setvclock <channel_id or none>```");
+
+    const id = args[0].toLowerCase() === "none" ? null : args[0];
+
+    client.db.config.lockVcChannel = id ? id : null;
+    saveDatabase(client.db);
+
+    message.channel.send("```js\nVoice channel lock set to " + (id ? id : "none") + "\n```");
+  }
+};
