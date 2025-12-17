ee337921a477cad0d58f131aaced1016b0398848
shoryadevraj <shoryadevraj@gmail.com>
2025-12-17 13:31:56 +0530

    Create setlavalink.js

---
+++ commands/utility/setlavalink.js
@@ -0,0 +1,19 @@
+export default {
+  name: "setlavalink",
+  aliases: ["lavalink"],
+  category: "utility",
+
+  async execute(message, args, client) {
+    if (args.length !== 3) return message.channel.send("```Usage: setlavalink <rest> <ws> <password>```");
+
+    client.db.config.lavalinkRest = args[0];
+    client.db.config.lavalinkWs = args[1];
+    client.db.config.lavalinkPassword = args[2];
+    saveDatabase(client.db);
+
+    // Reload Lavalink with new config
+    client.reloadLavalink();
+
+    message.channel.send("```js\nLavalink updated and reloaded\n```");
+  }
+};
