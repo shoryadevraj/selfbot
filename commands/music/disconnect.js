db4cab758b9f40d469d97ae06c72a52a91788add
shoryadevraj <shoryadevraj@gmail.com>
2025-12-17 15:44:33 +0530

    Update disconnect.js

--- commands/music/disconnect.js
+++ commands/music/disconnect.js
@@ -4,37 +4,24 @@ export default {
   name: 'disconnect',
   aliases: ['dc', 'leave'],
   category: 'music',
-  description: 'Disconnect from voice channel',
-  usage: 'disconnect',
+
   async execute(message, args, client) {
-    if (!message.guild) {
-      await message.channel.send('``````');
-      return;
-    }
-
     const guildId = message.guild.id;
-    
-    try {
-      // Destroy Lavalink player
-      await client.lavalink.destroyPlayer(guildId);
-      
-      // Clear queue
-      client.queueManager.delete(guildId);
-      
-      // Disconnect from voice channel
-      const connection = getVoiceConnection(guildId);
-      if (connection) {
-        connection.destroy();
-      }
-      
-      await message.channel.send('👍');
 
-      if (message.deletable) {
-        await message.delete().catch(() => {});
-      }
+    try {
+      await client.lavalink.destroyPlayer(guildId);
+      client.queueManager.delete(guildId);
+
+      const connection = getVoiceConnection(guildId);
+      if (connection) connection.destroy();
+
+      await message.react("👋");
+
+      if (message.deletable) message.delete().catch(() => {});
+
     } catch (err) {
       console.error('[DC Error]:', err);
-      await message.channel.send('``````');
+      message.react("❌").catch(() => {});
     }
-  },
+  }
 };
